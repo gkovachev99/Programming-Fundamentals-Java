cf4bc88fa6a2d82929d0937ec9b81d3b84d844a5
Georgi Kovachev <g_kovachev_99@yahoo.com>
2022-11-22 17:33:40 +0200

    Add Tasks

---
+++ BasicSyntax-Exercise/Task3.java
@@ -0,0 +1,12 @@
+package Labs;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char character = 'a';
+        int ascii = (int) character;
+        System.out.println(ascii);//97
+    }
+}
